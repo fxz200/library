05a6c19389ca4b4a0c4f684df0b65e9aa125b5d9
hedstby <boloily426@gmail.com>
2022-06-25 00:51:36 +0800

    20220625

---
+++ app/src/main/java/com/example/myapplication/qrcode.kt
@@ -0,0 +1,66 @@
+package com.example.myapplication
+
+import android.content.pm.PackageManager
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.SurfaceHolder
+import android.view.animation.Animation
+import android.widget.Toast
+import android.view.animation.AnimationUtils
+import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityManagerCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.example.myapplication.databinding.ActivityMainBinding
+import java.io.IOException
+import java.util.jar.Manifest
+
+
+class qrcode : AppCompatActivity() {
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qrcode)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+
+        codeScanner = CodeScanner(this, scannerView)
+
+        codeScanner.camera = CodeScanner.CAMERA_BACK
+        codeScanner.formats = CodeScanner.ALL_FORMATS
+        codeScanner.autoFocusMode = AutoFocusMode.SAFE
+        codeScanner.scanMode = ScanMode.SINGLE
+        codeScanner.isAutoFocusEnabled = true
+        codeScanner.isFlashEnabled = false
+
+        codeScanner.decodeCallback = DecodeCallback {
+            runOnUiThread{
+                Toast.makeText(this, "${it.text}", Toast.LENGTH_LONG).show()
+            }
+        }
+        codeScanner.errorCallback = ErrorCallback {
+            runOnUiThread{
+                Toast.makeText(this, "Error:${it.message}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+    override fun onResume(){
+        super.onResume()
+        codeScanner.startPreview()
+    }
+    override fun onPause(){
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+}
