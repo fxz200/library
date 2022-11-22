52aa3ce5ea013a36235805c874d78fc940f16494
hedstby <boloily426@gmail.com>
2022-11-22 20:41:00 +0800

    20221122

--- app/src/main/java/com/example/myapplication/login.kt
+++ app/src/main/java/com/example/myapplication/login.kt
@@ -1,11 +1,22 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+    fun tosignup(view: View) {
+        val intent = Intent(this,signup::class.java)
+        startActivity(intent)
+    }
+    fun tologin(view: View) {
+        val intent = Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
 }
