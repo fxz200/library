16b82ec83254693619dcd1f59763139ffccac3a1
fxz_2 <fxzxz200@gmail.com>
2022-12-14 14:45:50 +0800

    20221214

---
+++ app/src/main/java/com/example/myapplication/AR_test.kt
@@ -0,0 +1,17 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AR_test : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ar_test)
+    }
+
+
+
+
+
+
+}
